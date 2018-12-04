c189eddad08a627c78b28bccba018f39cd7bc524
Liviu <liviu.andron.work@gmail.com>
2018-12-04 13:38:13 +0100

    c++ samples/work started

---
+++ c++/start-cmake/hello.cpp
@@ -0,0 +1,12 @@
+/*
+    Reference
+        https://llvm.org/docs/GettingStartedVS.html
+        https://stackoverflow.com/questions/38171878/how-do-i-tell-cmake-to-use-clang-on-windows
+*/
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
