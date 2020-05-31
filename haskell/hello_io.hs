c3443547887def550a85a87c4b1d8881ed8c72b8
Liviu Andron <liviu.andron.work@gmail.com>
2020-05-31 16:50:21 +0200

    haskell re-started: doing I/O

---
+++ haskell/hello_io.hs
@@ -0,0 +1,54 @@
+{-
+  Purpose:
+   * get number of tests, T
+   * get the number of pairs, N, for the each tests
+   * get N (x, y) coordinates
+
+  Run
+	 * ghc --make -O2 ./test.hs
+   * runhaskell ./test.hs ?
+
+	Reference
+    In order to tell us what it calculated, it has to change the state of an output device
+     (usually the state of the screen), which then emits photons that travel to our brain
+     and change the state of our mind, man.
+ 	http://learnyouahaskell.com/input-and-output
+
+  TODO
+    * getLine vs readLn
+        see :t readLn
+    * runhaskell crash
+        "Access violation in generated code when reading 0xffffffffffffffff
+
+        Attempting to reconstruct a stack trace..."
+
+  #haskell_io #hs_io #hs_import
+  -}
+-- TODO: use Natural instead of Int
+import Numeric.Natural
+
+repeatNTimes 0 _ = return ()
+repeatNTimes n action = do
+  action
+  repeatNTimes (n-1) action
+
+{-
+  * define "main"
+  * do = do imperative
+      "in a do block, the last action cannot be bound to a name"
+  * <- vs let
+    "So use <- when you want to bind results of I/O actions to names
+    and you can use let bindings to bind pure expressions to names."
+  * return is sort of the opposite to <-.
+
+  * stuff: <-, ++, $, . (functions composition)
+-}
+main = do
+{-   putStrLn "Hello, what's your name?"
+   name <- getLine
+   putStrLn $ "Now give me a number, " ++ name ++ "!"-}
+   line <- getLine
+   -- Natural is better than Int
+   let no_pairs = (read line :: Natural)
+   -- print = putStrLn . show
+   print no_pairs
