2cd260b0fe881122ac6eafaf31546a8e6e8cb858
Liviu Andron <liviu.andron.work@gmail.com>
2020-07-19 22:10:44 +0200

    Modern cpp challenge: math library get_proper_divisors implemented

--- c++/102-modern-cpp-challenge/lib/math.cpp
+++ c++/102-modern-cpp-challenge/lib/math.cpp
@@ -6,6 +6,10 @@
 
 #include "utils.h"
 
+#include <set>
+
+using namespace math_lib;
+
 /*
     Reference
         proj_euler.py/get_primes_2
@@ -13,7 +17,7 @@
     Return value
         no cost: the vector will be just moved
 */
-math_lib::Primes math_lib::get_primes(unsigned limit)
+Primes math_lib::get_primes(unsigned limit)
 {
     if (limit < 3)
         return {};
@@ -44,7 +48,7 @@ math_lib::Primes math_lib::get_primes(unsigned limit)
     Reference
         proj_euler.py/get_prime_divisors
 */
-math_lib::Divisors math_lib::get_prime_divisors(unsigned number, const math_lib::Primes& primes)
+Divisors math_lib::get_prime_divisors(unsigned number, const Primes& primes)
 {
     const auto get_power = [](auto& number, auto divisor)
     {
@@ -76,6 +80,61 @@ math_lib::Divisors math_lib::get_prime_divisors(unsigned number, const math_lib:
     return divisors;
 }
 
+/*
+    tag_recursive_lambda
+    #recursive_lambda
+*/
+ProperDivisors math_lib::get_proper_divisors(const Divisors& prime_divisors)
+{
+    assert(!prime_divisors.empty());
+    //std::cout << "get_proper_divisors: " << to_string(prime_divisors) << std::endl;
+
+    using AllDivisors = std::set<Primes::value_type>;
+
+    //std::function<auto(auto,const Divisors&, size_t, auto, AllDivisors&) get_all_combinations;
+    const auto get_all_combinations =
+        [](const Divisors& prime_divisors, size_t where, auto current,
+           AllDivisors& result
+           )
+    {
+        const auto get_all_combinations_impl =
+        [](const Divisors& prime_divisors, size_t where, auto current,
+           AllDivisors& result, auto& get_all_combinations_func
+           ) -> void
+        {
+            using NumberType = std::remove_reference<decltype(current)>::type;
+
+            // 0 = without
+            for (NumberType i = 0, factor = 1; i <= prime_divisors[where].second;
+                 ++i, factor *= prime_divisors[where].first)
+            {
+                const auto next = current * factor;
+                //std::cout << "get_all_combinations: " << current << "," << factor << std::endl;
+                result.insert(next);
+                if (where + 1 < prime_divisors.size())
+                    get_all_combinations_func(prime_divisors, where+1, next,
+                        result, get_all_combinations_func);
+            }
+        };
+
+        return get_all_combinations_impl(prime_divisors, where, current,
+                    result, get_all_combinations_impl);
+    };
+
+    AllDivisors result{1};
+    get_all_combinations(prime_divisors, 0u, 1u, result);
+    ProperDivisors divisors;
+    for (auto i : result)
+    {
+        //std::cout << i << " ";
+        divisors.push_back(i);
+    }
+    // not the number itself
+    divisors.pop_back();
+    //std::cout << std::endl;
+    return divisors;
+}
+
 TEST(math, get_primes)
 {
     using namespace math_lib;
@@ -103,4 +162,16 @@ TEST(math, get_prime_divisors)
 
 /*    std::cout << "get_prime_divisors(100) "
               << to_string(get_prime_divisors(100, primes_1000)) << "." << std::endl;*/
+}
+
+TEST(math, get_proper_divisors)
+{
+    using namespace math_lib;
+    using Divs = Primes;
+
+    const auto primes_1000 = get_primes(1000);
+    ASSERT_EQ(Divs({1, 2, 3}), get_proper_divisors(get_prime_divisors(6, primes_1000)));
+    ASSERT_EQ(Divs({1, 2, 4}), get_proper_divisors(get_prime_divisors(8, primes_1000)));
+    ASSERT_EQ(Divs({1, 2, 3, 4, 6}), get_proper_divisors(get_prime_divisors(12, primes_1000)));
+    ASSERT_EQ(Divs({1, 2, 3, 4, 6, 8, 12}), get_proper_divisors(get_prime_divisors(24, primes_1000)));
 }
--- c++/102-modern-cpp-challenge/lib/math.h
+++ c++/102-modern-cpp-challenge/lib/math.h
@@ -8,7 +8,9 @@ namespace math_lib
 
 using Primes = std::vector<unsigned>;
 using Divisors = std::vector<std::pair<unsigned, unsigned>>;
+using ProperDivisors = Primes;
 
 Primes get_primes(unsigned);
 Divisors get_prime_divisors(unsigned, const Primes& primes);
+ProperDivisors get_proper_divisors(const Divisors& prime_divisors);
 }
