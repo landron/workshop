11bcf97ae33490541ff0308fcfb7d6cbd9006f2f
Liviu Andron <liviu.andron.work@gmail.com>
2021-03-14 13:50:17 +0100

    [C + cmake + libevent] the server works now: * strftime added for having the time * some bugs

--- c++/04-test-main-c/client.py
+++ c++/04-test-main-c/client.py
@@ -1,19 +1,27 @@
 import socket
-import struct
 import optparse
 
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 3333
+
+
 def prepare_sock(port):
-	udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	udp_sock.bind(('0.0.0.0', 0))
-	udp_sock.connect(('0.0.0.0', port))
-	return udp_sock
+    udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # not bblocking
+    # udp_sock.setblocking(0)
+    udp_sock.bind((HOST, 0))
+    udp_sock.connect((HOST, port))
+    return udp_sock
+
 
 if __name__ == '__main__':
-	parser = optparse.OptionParser()
-	parser.add_option('-p', '--port', dest='port', default=1497, type='int', help='The port to send to')
-	opts, args = parser.parse_args()
-	udp_sock = prepare_sock(opts.port)
-	udp_sock.send('')
-	buf = udp_sock.recv(100)
-	tv_sec, tv_usec = struct.unpack('@ll', buf)
-	print 'tv_sec = %d, tv_usec = %d' % (tv_sec, tv_usec)
+    parser = optparse.OptionParser()
+    parser.add_option('-p', '--port', dest='port', default=PORT,
+                      type='int', help='The port to send to')
+    opts, args = parser.parse_args()
+    udp_sock = prepare_sock(opts.port)
+    udp_sock.send(str.encode('I am a Python client.'))
+    udp_sock.settimeout(1.0)
+    buf = udp_sock.recv(100)
+    # print("Received: ", buf)
+    print(buf.decode("utf-8"))
--- c++/04-test-main-c/clock.c
+++ c++/04-test-main-c/clock.c
@@ -1,7 +1,7 @@
 /*
-	TODO
-		* warning C4028: formal parameter 1 different from declaration
-		the two event_new
+    TODO
+        * warning C4028: formal parameter 1 different from declaration
+        the two event_new
 */
 #ifndef _WIN32
 #include <netinet/in.h>
@@ -20,47 +20,60 @@
 
 #define DEFAULT_PORT 3333
 
-static struct tm g_clock;
+static char g_clock[128];
 
 static struct timeval TIMER_TV = {1, 0};
 
 static void gettimeofday_cb(int nothing, short int which, void *ev)
 {
-	// if (gettimeofday(&CLOCK_TV, NULL)) {
-	// 	perror("gettimeofday()");
-	// 	event_loopbreak();
-	// }
+    time_t rawtime;
+    time(&rawtime);
 
-	evtimer_add(ev, &TIMER_TV);
+    struct tm local_snapshot;
+#if (defined(__MINGW32__) || defined(__MINGW64__))
+    memcpy(&local_snapshot, localtime(&rawtime), sizeof(tm));
+#elif (defined(WIN32) || defined(_WIN32) || defined(__WIN32__))
+    localtime_s(&local_snapshot, &rawtime);
+#else
+    localtime_r(&rawtime, &local_snapshot); // POSIX
+#endif
+
+    strftime(g_clock, sizeof(g_clock), "%d/%m/%YT%T", &local_snapshot);
+
+    evtimer_add(ev, &TIMER_TV);
 }
 
 static void udp_cb(const int sock, short int which, void *arg)
 {
-	struct sockaddr_in server_sin;
-	socklen_t server_sz = sizeof(server_sin);
-	struct event_base *base = arg;
-	char buf[1024];
+    struct sockaddr_in server_sin;
+    socklen_t server_sz = sizeof(server_sin);
+    struct event_base *base = arg;
+    char buf[1024];
 
-	/* Recv the data, store the address of the sender in server_sin */
-	int size = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr *) &server_sin, &server_sz);
-	if (size == -1) {
-		perror("recvfrom()");
-		event_base_loopbreak(base);
-	}
-	buf[size] = '\0';
+    /* Recv the data, store the address of the sender in server_sin */
+    int size = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr *) &server_sin, &server_sz);
+    if (size == -1) {
+        perror("recvfrom()");
+        event_base_loopbreak(base);
+    }
+    buf[size] = '\0';
 
-	/* Copy the time into buf; note, endianess unspecified! */
-	// memcpy(buf, &CLOCK_TV, sizeof(CLOCK_TV));
+    printf("[udp_cb] client said: %s\n", buf);
 
-	/* Send the data back to the client */
-	if (sendto(sock, buf, (int)strlen(buf), 0, (struct sockaddr *) &server_sin, server_sz) == -1 ) {
-		perror("sendto()");
-		event_base_loopbreak(base);
-	}
+    /* Copy the time into buf; note, endianess unspecified! */
+    memcpy(buf, g_clock, strlen(g_clock));
+
+    /* Send the data back to the client */
+    if (sendto(sock, buf, (int)strlen(buf), 0, (struct sockaddr *) &server_sin, server_sz) == -1 ) {
+        perror("sendto()");
+        event_base_loopbreak(base);
+    }
 }
 
 bool server()
 {
+    g_clock[0] = '\0';
+
 #ifndef _WIN32
     int sock;
 #else
@@ -72,42 +85,45 @@ bool server()
     WSAStartup(0x0201, &wsa_data);
 #endif
 
-    struct event_base *base_timer, *base_udp;
-	struct event *timer_event, *udp_event;
-	struct sockaddr_in sin;
+    struct event_base *base;
+    struct event *timer_event, *udp_event;
+    struct sockaddr_in sin;
 
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	memset(&sin, 0, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = INADDR_ANY;
-	sin.sin_port = htons(1497);
+    int port = DEFAULT_PORT;
 
-	if (bind(sock, (struct sockaddr *) &sin, sizeof(sin))) {
-		perror("bind()");
-		// exit(EXIT_FAILURE);
-		return false;
-	}
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(port);
 
-	/* Initialize libevent */
-	//	DEPRECATED: replaced with event_base_new
-	//event_init();
-	base_timer = event_base_new();
-	base_udp = event_base_new();
+    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin))) {
+        perror("bind()");
+        // exit(EXIT_FAILURE);
+        return false;
+    }
 
-	/* Add the clock event */
-	timer_event = evtimer_new(base_timer, &gettimeofday_cb, event_self_cbarg());
-	evtimer_add(timer_event, &TIMER_TV);
+    printf("Clock server started at %d ...\n", port);
 
-	/* Add the UDP event */
-	udp_event = event_new(base_udp, sock, EV_READ|EV_PERSIST, udp_cb, event_self_cbarg());
-	event_add(udp_event, 0);
+    /* Initialize libevent */
+    //  DEPRECATED: replaced with event_base_new
+    //event_init();
+    base = event_base_new();
 
-	/* Enter the event loop; does not return. */
-	event_base_dispatch(base_timer);
-	event_base_dispatch(base_udp);
+    /* Add the clock event */
+    timer_event = evtimer_new(base, &gettimeofday_cb, event_self_cbarg());
+    evtimer_add(timer_event, &TIMER_TV);
 
-	event_free(udp_event);
-	event_free(timer_event);
+    /* Add the UDP event */
+    udp_event = event_new(base, sock, EV_READ|EV_PERSIST, udp_cb, event_self_cbarg());
+    event_add(udp_event, 0);
+
+    /* Enter the event loop; does not return. */
+    event_base_dispatch(base);
+
+    event_free(udp_event);
+    event_free(timer_event);
+    event_base_free(base);
 
 #ifndef _WIN32
     close(sock);
@@ -116,8 +132,8 @@ bool server()
 #endif
 
 #ifdef _WIN32
-	WSACleanup();
+    WSACleanup();
 #endif
 
-	return true;
+    return true;
 }
