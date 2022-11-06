68778b090a4d0ddf1cd615be51e30ba659a78481
Liviu@Ubuntu <liviu.andron.work@gmail.com>
2022-11-06 23:01:06 +0100

    go tour: concurrency exercise reviewed
    Wait for the work to finish with sync.WaitGroup

--- go/Basic/Tour/ex7_concurrency.go
+++ go/Basic/Tour/ex7_concurrency.go
@@ -3,7 +3,12 @@
 	https://tour.golang.org/concurrency/10
 
 	https://stackoverflow.com/questions/18207772/how-to-wait-for-all-goroutines-to-finish-without-using-time-sleep
-		use sync.WaitGroup
+	https://stackoverflow.com/questions/13217547/tour-of-go-exercise-10-crawler
+		using sync.WaitGroup seems the better solution
+
+	TODO
+		* add a command line paramater to switch between solutions
+		* export some statistics to have some results assertions (4 found, 1 not found, 6 hits)
 */
 package main
 
@@ -43,8 +48,9 @@ func (c *Cache) Add(key string) bool {
 
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl_rec(url string, depth int, fetcher Fetcher,
-	cache Cache, quit_parent chan struct{}) {
+func Crawl_rec1(url string, depth int, fetcher Fetcher, cache Cache,
+	quit_parent chan struct{}) {
+
 	defer func(quit chan struct{}) {
 		quit <- struct{}{}
 	}(quit_parent)
@@ -63,23 +69,160 @@ func Crawl_rec(url string, depth int, fetcher Fetcher,
 	fmt.Printf("found: %s %q\n", url, body)
 	quit_children := make(chan struct{})
 	for _, u := range urls {
-		go Crawl_rec(u, depth-1, fetcher, cache, quit_children)
+		go Crawl_rec1(u, depth-1, fetcher, cache, quit_children)
 	}
 	for i := 0; i < len(urls); i++ {
 		<-quit_children
 	}
 }
 
-func Crawl_conc(url string, depth int, fetcher Fetcher) {
-	cache := Cache{v: make(map[string]struct{})}
+// Wait for work to finish: sync goroutines with channels (one for each goroutine)
+func Crawl_solution1(url string, depth int, fetcher Fetcher) {
 	quit := make(chan struct{})
-	go Crawl_rec(url, depth, fetcher, cache, quit)
+
+	cache := Cache{v: make(map[string]struct{})}
+	go Crawl_rec1(url, depth, fetcher, cache, quit)
 	<-quit
 }
 
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl_rec2(url string, depth int, fetcher Fetcher, cache Cache,
+	work_done *sync.WaitGroup) {
+
+	defer func(work *sync.WaitGroup) {
+		work.Done()
+	}(work_done)
+
+	if depth <= 0 {
+		return
+	}
+	if !cache.Add(url) {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+
+	for _, u := range urls {
+		work_done.Add(1)
+		go Crawl_rec2(u, depth-1, fetcher, cache, work_done)
+	}
+}
+
+// Wait for work to finish: sync.WaitGroup
+/*
+	Warnings:
+		* WaitGroup must be pointer, otherwise it will create copies when
+		calling functions and deadlock
+		* Add must be called before calling the goroutine
+			see comments: https://stackoverflow.com/a/18207832/1032277
+
+	Other solutions/variants
+		* each goroutine with its own WaitGroup => no arguments needed
+		https://gist.github.com/danimal141/09ed828ce4d676faa6fa9f19aeea2867
+
+	Reference
+		https://stackoverflow.com/questions/18207772/how-to-wait-for-all-goroutines-to-finish-without-using-time-sleep
+*/
+func Crawl_solution2(url string, depth int, fetcher Fetcher) {
+	// var quit sync.WaitGroup
+	// quit := new(sync.WaitGroup)
+	quit := &sync.WaitGroup{}
+
+	cache := Cache{v: make(map[string]struct{})}
+	quit.Add(1)
+	go Crawl_rec2(url, depth, fetcher, cache, quit)
+	quit.Wait()
+}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl3(url string, depth int, fetcher Fetcher, cache Cache) {
+	if depth <= 0 {
+		return
+	}
+	if !cache.Add(url) {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+
+	var work_done sync.WaitGroup
+	for _, u := range urls {
+		work_done.Add(1)
+		go func(u string) {
+			defer work_done.Done()
+			Crawl3(u, depth-1, fetcher, cache)
+		}(u)
+	}
+	work_done.Wait()
+}
+
+/*
+	like Crawl_solution2, but creating one sync.WaitGroup
+	for each goroutine group creation
+*/
+func Crawl_solution3(url string, depth int, fetcher Fetcher) {
+	cache := Cache{v: make(map[string]struct{})}
+	Crawl3(url, depth, fetcher, cache)
+}
+
+//	Like Crawl_solution2, but keeping functions declarations (parameters)
+//	from the exercise.
+func Crawl4(url string, depth int, fetcher Fetcher) {
+	cache := &Cache{v: make(map[string]struct{})}
+	done := &sync.WaitGroup{}
+
+	var worker func(url string, depth int, fetcher Fetcher,
+		cache *Cache, done *sync.WaitGroup)
+
+	worker = func(url string, depth int, fetcher Fetcher,
+		cache *Cache, done *sync.WaitGroup) {
+		defer done.Done()
+
+		if depth <= 0 {
+			return
+		}
+		if !cache.Add(url) {
+			return
+		}
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("found: %s %q\n", url, body)
+
+		for _, u := range urls {
+			done.Add(1)
+			go worker(u, depth-1, fetcher, cache, done)
+		}
+	}
+
+	done.Add(1)
+	go worker(url, depth, fetcher, cache, done)
+	done.Wait()
+}
+
+/*
+	Other solutions/variants
+		* each goroutine with its own WaitGroup => no arguments needed
+		https://gist.github.com/danimal141/09ed828ce4d676faa6fa9f19aeea2867
+*/
 func main() {
-	//Crawl("https://golang.org/", 4, fetcher)
-	Crawl_conc("https://golang.org/", 4, fetcher)
+	// Crawl_solution1("https://golang.org/", 4, fetcher)
+
+	// Crawl_solution3("https://golang.org/", 4, fetcher)
+	// Crawl_solution2("https://golang.org/", 4, fetcher)
+	Crawl4("https://golang.org/", 4, fetcher)
 }
 
 // fakeFetcher is Fetcher that returns canned results.
