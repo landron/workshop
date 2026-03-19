759477c310dcb4cbe04f41b16f61010f47b22603
Liviu@Manjaro <liviu.andron.work@gmail.com>
2026-03-19 19:11:02 +0100

    CMake basic test updated to C++23

--- c++/03-test-main/test.cpp
+++ c++/03-test-main/test.cpp
@@ -1,7 +1,9 @@
 
-#include <iostream>
+// std::cout is "Heavy": The <iostream> library is notorious for slow compile times and a clunky interface.
+// #include <iostream>
+#include <print>
 
-void main()
-{
-    std::cout << "Hello World!" << std::endl;
+int main() {
+    std::println("Hello World!"); 
+    // std::println automatically adds the newline and is more efficient than cout.
 }
