135b10dccf355f86d472f89b70f6d01691bf87a2
Liviu@Manjaro <liviu.andron.work@gmail.com>
2026-03-20 04:50:53 +0100

    C++17 copy-elision test: GTest independent

--- c++/11-tests-collection/cpp17/copy-elision/main.cpp
+++ c++/11-tests-collection/cpp17/copy-elision/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 
+#ifndef TEST_CPP_14
 #include "gtest/gtest.h"
+#else
+#include <cassert>
+#define ASSERT_TRUE(expr) assert(expr)
+#endif
 //  copy elision
 #include "NonMoveable.h"
 
+#ifndef TEST_CPP_14
 TEST(test_sample, test1)
+#else
+int main()
+#endif
 {
     const auto largeNonMovableObj = make();
     ASSERT_TRUE(largeNonMovableObj.arr.size() > 30);
